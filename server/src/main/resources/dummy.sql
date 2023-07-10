57b0d0491621640857e57f0dd40cfe04d7b96b16
minit88 <chymh32@gmail.com>
2023-07-10 23:33:33 +0900

    ✨  sql 설정

--- server/src/main/resources/dummy.sql
+++ server/src/main/resources/dummy.sql
@@ -1,2 +1,7 @@
 INSERT INTO Member (email, display_name, latitude, longitude)
 VALUES ('test@test.com', '둘리', 37.5793493362539, 126.91794995956589);
+
+INSERT INTO Product (product_id, title, content, base_Fee, fee_per_day, overdue_fee, minimum_rental_period, total_Rate_Score, total_Rate_Count, view_Count, latitude, longitude, member_id)
+VALUES ('product_id_value', '제목입니다.', '내용입니다.', 1000, 100, 200, 7, 0, 0, 0, 37.5793493362539, 126.91794995956589, 1);
+INSERT INTO Product (product_id, title, content, base_Fee, fee_per_day, overdue_fee, minimum_rental_period, total_Rate_Score, total_Rate_Count, view_Count, latitude, longitude, member_id)
+VALUES ('product_id_valued', '제목입니다.', '내용입니다.', 1000, 100, 200, 7, 0, 0, 0, 37.5793493362539, 126.91794995956589, 1);
